f1c971150d272fe3b2e156f8a72b23ae3c1ded29
Guilherme Rodrigues <guilher.c.rodrigues@gmail.com>
2025-03-07 14:36:45 -0300

    project init

---
+++ internal/server/server.go
@@ -0,0 +1,40 @@
+package server
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+
+	_ "github.com/joho/godotenv/autoload"
+
+	"mineServers/internal/database"
+)
+
+type Server struct {
+	port int
+	ctx  context.Context
+	db   database.Service
+}
+
+func NewServer() *http.Server {
+	port, _ := strconv.Atoi(os.Getenv("PORT"))
+	NewServer := &Server{
+		port: port,
+
+		db: database.New(),
+	}
+
+	// Declare Server config
+	server := &http.Server{
+		Addr:         fmt.Sprintf(":%d", NewServer.port),
+		Handler:      NewServer.RegisterRoutes(),
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+
+	return server
+}
